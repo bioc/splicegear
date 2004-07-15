1e585e4745feb6cac94fe31bbab07cb3e3b8cc8d
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-07-15 19:39:57 +0000

    changing the inherits to is
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/splicegear@9435 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/as.data.frame.SpliceSites.R
+++ R/as.data.frame.SpliceSites.R
@@ -1,8 +1,8 @@
 as.data.frame.SpliceSites <- function(x, row.names=NA, optional=NA) {
-  
-  if (! inherits(x, "SpliceSites"))
-    stop("the argument should inherit class 'SpliceSites'")
-  
+
+    if (! is(x, "SpliceSites"))
+        stop("the argument should inherit class 'SpliceSites'")
+
   nr.typeII <- length(x@spsiteIIpos)
 
   pdat.I <- x@spsiteIpos.pData@pData
@@ -19,13 +19,13 @@ as.data.frame.SpliceSites <- function(x, row.names=NA, optional=NA) {
   } else {
     m <- merge(pdat.I, pdat.II, by.x = c(0, seq(along=pdat.I)), by.y = c(0, seq(along=pdat.II)), all = TRUE)
   }
-  
+
   rv <- do.call("data.frame", c(list(begin = c(x@spsiteIpos[, 1], x@spsiteIIpos),
                                      end = c(x@spsiteIpos[, 2], rep(NA, nr.typeII)),
                                      lapply(m, function(x) {x})
                                      ))
                 )
-  
-  
+
+
   return(rv)
 }
